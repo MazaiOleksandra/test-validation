b22dbf2020a5a8fd198cb66a9f2cf03fcad301c8
MazaiOleksandra <43841829+MazaiOleksandra@users.noreply.github.com>
2019-01-29 13:30:47 +0200

    Add files via upload

---
+++ content.php
@@ -0,0 +1,92 @@
+<div class="container-fluid" id="main_menu">
+    <div class="nav" align="center">
+        <button id="tab_main" class="main_tabs">
+            Home
+        </button>
+        <button id="tab_prof" class="main_tabs">
+            Profile
+        </button>
+        <button id="tab_statistic" class="main_tabs">
+            Statistic
+        </button>
+    </div>
+
+</div>
+
+
+<div id="main_table" class="container-fluid">
+
+    <?php
+
+
+    $query = 'select count(*) as cnt
+,Category from tQueueTasks
+where FinishDT is null 
+group by Category';
+    $conn = pg_connect("host=ec2-23-21-171-25.compute-1.amazonaws.com
+ dbname=d4veaugad1osfk user=wklzxhhlnzvaqs password=8486dd5267e33b69124f5e83d9773d5d6e56a3455a5011036a0238d2f3f3c11a");
+    $result = pg_fetch_all(pg_query($conn, $query));
+    pg_close($conn);
+
+    $i = 0;
+    $ii = count($result);
+    echo '<div class="block">';
+    for ($i; $i < $ii; $i++) {
+        $category = $result[$i]['category'];
+        $cnt = $result[$i]['cnt'];
+
+        echo '<div id="blocks_control_panel_params" class="control_panel">
+    <table width="1000px" class="panel_table padding_less">
+        <thead>
+        <tr>
+            <th colspan="2">' . $category . '</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Очередь - ' . $cnt . '</td> 
+            <td style="text-align: right">
+                <button class="startBtn" id="clid_main_btn" onclick="startValidation(\'' . $category . '\')">
+                    Start
+                </button>
+            </td>           
+        </tr>
+     
+        </tbody>
+    </table>
+</div> ';
+
+    };
+    ?>
+
+</div>
+</div>
+
+
+<div id="validationQueue" class="container-fluid">
+
+
+    <div class="response_validation" id="main_table_pm"></div>
+
+
+</div>
+
+
+<div id="log">
+    <div class="container-fluid">
+        <div class="ptp_result requests_table" id="precalc_svc_requests_table"></div>
+        <div class="ptp_result requests_table" id="full_svc_requests_table"></div>
+    </div>
+</div>
+
+
+<div id="prof">
+    <div class="container-fluid">
+       профайл
+    </div>
+</div></div>
+
+<div id="statistic">
+    statistic
+</div>
+
---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <link rel="shortcut icon" href="/lib/img/pb_shadow.gif"/>
+<!--    <link rel="stylesheet" href="/lib/js/jquery-ui-1.11.4/jquery-ui.min.css"/>-->
+<!--    <link rel="stylesheet" href="/lib/bootstrap-3.3.7/css/bootstrap.min.css"/>-->
+<!--    <link rel="stylesheet" href="/lib/bootstrap-3.3.6/css/bootstrap-select.min.css"/>-->
+<!--    <link rel="stylesheet" type="text/css" href="/lib/css/style.css"/>-->
+    <link rel="stylesheet" href="styles.css"/>
+
+    <script src="functions.js"></script>
+    <script src="/lib/js/functions.js"></script>
+    <script src="/lib/js/jquery-3/jquery-3.1.1.min.js"></script>
+    <script src="/lib/js/jquery-migrate-3.0.0.js"></script>
+    <script src="/lib/js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    <script src="/lib/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+    <script src="/lib/bootstrap-3.3.6/js/bootstrap-select.js"></script>
+    <script src="/lib/tablesorter-2.27.7/js/jquery.tablesorter.js"></script>
+    <script src="/lib/tablesorter-2.27.7/js/jquery.tablesorter.widgets.js"></script>
+    <script type="text/javascript" src="/lib/js/jquery.blockUI.js"></script>
+
+</head>
+<body class="grayhtml">
+<?php
+include "content.php";
+?>
+
+<script>
+    Init();
+</script>
+
+</body>
+</html>
+
+
