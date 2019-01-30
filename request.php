67680eff65b231814e7f8e4ecf2c24c6827075b8
MazaiOleksandra <43841829+MazaiOleksandra@users.noreply.github.com>
2019-01-30 16:51:14 +0200

    Update request.php

--- request.php
+++ request.php
@@ -6,7 +6,7 @@ if ($type=='updateRes'){
     $id = $_REQUEST['id'];
     $isValid = $_REQUEST['res'];
     $queryUpdate = "update tQueueTasks set finishdt=now(),isvalid ='$isValid' where ID=$id";
-    $conn = pg_connect("host=ec2-23-21-171-25.compute-1.amazonaws.com
+    $conn =pg_connect("host=ec2-23-21-171-25.compute-1.amazonaws.com
  dbname=d4veaugad1osfk user=wklzxhhlnzvaqs password=8486dd5267e33b69124f5e83d9773d5d6e56a3455a5011036a0238d2f3f3c11a");
     $resultUpdate = pg_fetch_all(pg_query($conn, $queryUpdate));
     $result = 0;
@@ -20,7 +20,7 @@ else {
 
 
     $queryQuestion = "select 
-coalesce(Question ,'aren\'t question. foto valid?')as Question ,
+coalesce(Question ,'aren\t question. foto valid?')as Question ,
 ID,
 Path2File,
 coalesce(CorrectVariant,'1') as CorrectVariant
@@ -34,20 +34,17 @@ coalesce(CorrectVariant,'1') as CorrectVariant
                               LIMIT 1
     ) b on a.Category=b.Category";
 
-    $conn = pg_connect("host=ec2-23-21-171-25.compute-1.amazonaws.com
+    $conn =pg_connect("host=ec2-23-21-171-25.compute-1.amazonaws.com
  dbname=d4veaugad1osfk user=wklzxhhlnzvaqs password=8486dd5267e33b69124f5e83d9773d5d6e56a3455a5011036a0238d2f3f3c11a");
     $cnt = pg_fetch_all(pg_query($conn, $queryCnt));
-              var_dump($cnt);
-
-    if ($cnt[0]['count'] != 0) {
+    if ($cnt[0]['count'] != 0) { //поменять на 0
         $result = pg_fetch_all(pg_query($conn, $queryQuestion));
-                      var_dump($result);
-
 
         $currTask = $result[0]['id'];
         $queryUpdate = "update tQueueTasks set StartDT=now() where ID=$currTask";
         $resultUpdate = pg_fetch_all(pg_query($conn, $queryUpdate));
 
+        //  var_dump($result);
     } else {
         $result = 0;
 
@@ -59,7 +56,6 @@ coalesce(CorrectVariant,'1') as CorrectVariant
 }
 
 $data =$result;
-
 
 $data = json_encode($data);
 die($data);
